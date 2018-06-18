5b549b80842f63140292c4bfaf8f2f59b24ba538
Daniel Saukel <sa.da@unitybox.de>
2018-06-18 22:57:03 +0200

    Implement default setAttribute method for new versions

--- core/src/main/java/de/erethon/commons/item/New.java
+++ core/src/main/java/de/erethon/commons/item/New.java
@@ -16,6 +16,7 @@ import de.erethon.commons.compatibility.CompatibilityHandler;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Random;
 import org.bukkit.inventory.ItemStack;
 
 /**
@@ -41,7 +42,17 @@ class New extends InternalsProvider {
     public static Class NBT_BASE;
 
     public static Class NBT_TAG_COMPOUND;
+    public static Method NBT_TAG_COMPOUND_GET_LIST;
     public static Method NBT_TAG_COMPOUND_SET;
+
+    public static Class NBT_TAG_BYTE;
+    public static Constructor NBT_TAG_BYTE_CONSTRUCTOR;
+
+    public static Class NBT_TAG_DOUBLE;
+    public static Constructor NBT_TAG_DOUBLE_CONSTRUCTOR;
+
+    public static Class NBT_TAG_INT;
+    public static Constructor NBT_TAG_INT_CONSTRUCTOR;
 
     public static Class NBT_TAG_LIST;
     public static Method NBT_TAG_LIST_ADD;
@@ -53,8 +64,18 @@ class New extends InternalsProvider {
         try {
             NBT_BASE = Class.forName(NET_MINECRAFT_SERVER + ".NBTBase");
 
+            NBT_TAG_BYTE = Class.forName(NET_MINECRAFT_SERVER + ".NBTTagByte");
+            NBT_TAG_BYTE_CONSTRUCTOR = NBT_TAG_BYTE.getConstructor(byte.class);
+
             NBT_TAG_COMPOUND = Class.forName(NET_MINECRAFT_SERVER + ".NBTTagCompound");
+            NBT_TAG_COMPOUND_GET_LIST = NBT_TAG_COMPOUND.getDeclaredMethod("getList", String.class, int.class);
             NBT_TAG_COMPOUND_SET = NBT_TAG_COMPOUND.getDeclaredMethod("set", String.class, NBT_BASE);
+
+            NBT_TAG_DOUBLE = Class.forName(NET_MINECRAFT_SERVER + ".NBTTagDouble");
+            NBT_TAG_DOUBLE_CONSTRUCTOR = NBT_TAG_DOUBLE.getConstructor(double.class);
+
+            NBT_TAG_INT = Class.forName(NET_MINECRAFT_SERVER + ".NBTTagInt");
+            NBT_TAG_INT_CONSTRUCTOR = NBT_TAG_INT.getConstructor(int.class);
 
             NBT_TAG_LIST = Class.forName(NET_MINECRAFT_SERVER + ".NBTTagList");
             NBT_TAG_LIST_ADD = NBT_TAG_LIST.getDeclaredMethod("add", NBT_BASE);
@@ -76,7 +97,33 @@ class New extends InternalsProvider {
 
     @Override
     ItemStack setAttribute(ItemStack itemStack, String attributeName, double amount, byte operation, String... slots) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            Object nmsStack = CRAFTITEMSTACK_AS_NMS_COPY.invoke(null, itemStack);
+
+            Object compound = ITEMSTACK_GET_TAG.invoke(nmsStack);
+            if (compound == null) {
+                compound = NBT_TAG_COMPOUND.newInstance();
+            }
+
+            Object modifiers = NBT_TAG_COMPOUND_GET_LIST.invoke(compound, "AttributeModifiers", 10);
+            for (String slot : slots) {
+                Object attribute = NBT_TAG_COMPOUND.newInstance();
+                NBT_TAG_COMPOUND_SET.invoke(attribute, "AttributeName", NBT_TAG_STRING_CONSTRUCTOR.newInstance(attributeName));
+                NBT_TAG_COMPOUND_SET.invoke(attribute, "Name", NBT_TAG_STRING_CONSTRUCTOR.newInstance(attributeName));
+                NBT_TAG_COMPOUND_SET.invoke(attribute, "Amount", NBT_TAG_DOUBLE_CONSTRUCTOR.newInstance(amount));
+                NBT_TAG_COMPOUND_SET.invoke(attribute, "Operation", NBT_TAG_BYTE_CONSTRUCTOR.newInstance(operation));
+                NBT_TAG_COMPOUND_SET.invoke(attribute, "UUIDLeast", NBT_TAG_INT_CONSTRUCTOR.newInstance(new Random().nextInt(50000) + 1));
+                NBT_TAG_COMPOUND_SET.invoke(attribute, "UUIDMost", NBT_TAG_INT_CONSTRUCTOR.newInstance(new Random().nextInt(100000) + 50001));
+                NBT_TAG_COMPOUND_SET.invoke(attribute, "Slot", NBT_TAG_STRING_CONSTRUCTOR.newInstance(slot));
+                NBT_TAG_LIST_ADD.invoke(modifiers, attribute);
+            }
+
+            ITEMSTACK_SET_TAG.invoke(nmsStack, compound);
+            return (ItemStack) CRAFTITEMSTACK_AS_BUKKIT_COPY.invoke(null, nmsStack);
+
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException exception) {
+            return itemStack;
+        }
     }
 
     @Override
