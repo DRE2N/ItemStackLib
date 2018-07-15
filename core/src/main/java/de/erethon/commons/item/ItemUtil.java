a9a366f4bcba02abf980970f1a2754eb8cb8a893
Daniel Saukel <sa.da@unitybox.de>
2018-07-15 03:42:02 +0200

    Update to 1.13

--- core/src/main/java/de/erethon/commons/item/ItemUtil.java
+++ core/src/main/java/de/erethon/commons/item/ItemUtil.java
@@ -30,6 +30,9 @@ public class ItemUtil {
             case NEW:
                 internals = new New();
                 break;
+            case v1_13_R1:
+                internals = new v1_13_R1();
+                break;
             case v1_12_R1:
                 internals = new v1_12_R1();
                 break;
